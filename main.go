cff35b60bf2cc7dcee4724ceb4b03fd0fcac4776
Gavin152 <nikolas.molinari@gmail.com>
2024-08-05 21:13:54 +0200

    added Update implementation

--- main.go
+++ main.go
@@ -20,3 +20,29 @@ func initialModel() model {
 func (m model) Init() tea.Cmd {
 	return nil
 }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor == len(m.items)-1 {
+				m.cursor++
+			}
+		case "enter", "l":
+			_, ok := m.checked[m.cursor]
+			if ok {
+				delete(m.checked, m.cursor)
+			} else {
+				m.checked[m.cursor] = true
+			}
+		}
+	}
+	return m, nil
+}
