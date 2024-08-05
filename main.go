e8e587aa87c7c2152374b083c9f80b366b6409fc
Gavin152 <nikolas.molinari@gmail.com>
2024-08-05 21:51:48 +0200

    add lipgloss for more bling

--- main.go
+++ main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"os"
 )
 
 type model struct {
-	items   []string
-	cursor  int
-	checked map[int]bool
+	items    []string
+	cursor   int
+	checked  map[int]bool
+	viewport viewport.Model
 }
 
 func initialModel() model {
+	vp := viewport.New(80, 10)
+	vp.Style = lipgloss.NewStyle().
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("#009688")).
+		PaddingLeft(2)
 	return model{
-		items:   []string{"Zirbe", "Schopf", "Erdäpfel", "Krenn", "Senf"},
-		cursor:  0,
-		checked: make(map[int]bool),
+		items:    []string{"Zirbe", "Schopf", "Erdäpfel", "Krenn", "Senf"},
+		cursor:   0,
+		checked:  make(map[int]bool),
+		viewport: vp,
 	}
 }
 
@@ -67,8 +76,9 @@ func (m model) View() string {
 		uiString += fmt.Sprintf("%s [%s] %s\n", cursor, checked, item)
 	}
 
-	uiString += fmt.Sprintf("\nPress 'q' or 'crtl+x' to quit\n")
-	return uiString
+	uiString += fmt.Sprintf("\nPress 'q' or 'crtl+x' to quit")
+	m.viewport.SetContent(uiString)
+	return m.viewport.View()
 }
 
 func main() {
